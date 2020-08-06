160f14e6a4057e59c4f1e6293e6eb574a28f93fd
kunxin-chor <chorkunxin@yahoo.com>
2020-08-06 05:04:22 +0000

    Added questions

---
+++ queries-questions.md
@@ -0,0 +1,31 @@
+# TRANCHE 1
+
+1. Find all the customers whose credit limit is more than 20K.
+
+2. Find all the customers whose credit limit is more than 20K and are from USA
+
+3. Find all customers whose name include the string `gift`
+
+4. Find all customers who matches one of the critera below:
+    * are from France  
+    * are from the USA and have less than 20K credit limit
+
+5. For all customers, list their customer number and customer name, along with the employee who is their sales rep.
+
+# TRANCHE 2
+
+1. Find all the products that are from the `Welly Diecast Productions` vendor.
+
+2. Find all the products that have the substring `car` inside its product name.
+
+3. Find all the products that have a buy price between 50.00 and 80.00
+
+4. Find all the products that have the substring `car` inside its product name and have a buy price
+between 50.00 and 80.00 
+
+5. If a customer wants to buy 7000 units of a particular product but is not willing to spend more than $50,
+what are the products they should consider?
+
+6. For each product, displays the text description associated with its product line.
+
+
--- queries.sql
+++ queries.sql
@@ -89,3 +89,93 @@ create table appointments (
 alter table appointments add preceding_appointment_id int unsigned;
 alter table appointments add constraint fk_preceding_appointment_id 
     foreign key (preceding_appointment_id)  references appointments(appointment_id);
+
+
+/** library schema ** /
+
+/* 1. create database */
+create database library;
+
+use library;
+
+create table books (
+    book_id int unsigned auto_increment primary key,
+    title varchar(200) not null
+);
+
+create table authors (
+    author_id int unsigned auto_increment primary key,
+    fname varchar(50) not null,
+    lname varchar(50) not null,
+    date_of_birth datetime
+);
+
+alter table books engine=InnoDB;
+alter table authors engine=InnoDB;
+
+create table writing_credits (
+    writing_credit_id int unsigned auto_increment primary key,
+    author_id int unsigned not null,
+    foreign key (author_id) references authors (author_id) on delete cascade not,
+    book_id int unsigned not null,
+    foreign key (book_id) references books (book_id) on delete cascade
+) engine = InnoDB;
+
+
+insert into authors (fname, lname) values ("John", "Ronald Tolkien"),
+                                          ("Clark", "Lewis Staple"),
+                                          ("Frank", "Hebret"),
+                                          ("John", "Doe"),
+                                          ("Jane", "Smith");
+
+insert into books (title) values ("Chronicles of Narina"),
+                                ("Lord of the Rings, The"),
+                                ("Generic Textbook"),
+                                ("Dune");
+
+insert into writing_credits (author_id, book_id) values (1, 2);
+insert into writing_credits (author_id, book_id) values (2, 1), (3, 4), (4, 3), (5, 3);
+
+/* see all books and their authors */
+select books.book_id, authors.author_id, title, fname, lname from books
+     join writing_credits on 
+        books.book_id = writing_credits.book_id 
+     join authors on
+        writing_credits.author_id = authors.author_id;
+
+delete from authors where author_id = 5;
+
+/* copies */
+create table copies (
+    copy_id int unsigned auto_increment primary key,
+    qty tinyint not null default 1,
+    book_id int unsigned,
+    foreign key(book_id) references books (book_id) on delete set null
+) engine=InnoDB;
+
+insert into copies (book_id, quality) values (1, 5), (1, 3), (2, 3), (2, 3), (2,3), (3,1);
+
+/* for the classicmodels db */
+/* select all rows but with specific columns*/
+select firstName, lastName, email from employees;
+
+/* select rows with specific critera */
+select * from employees where officeCode=1;
+select * from employees where emplyoeeNumber=1002;
+
+/* compound critera */
+select * from employees WHERE officeCode=4 and jobTitle="Sales Rep"
+select * from employees where officeCode = 1 or officeCode = 4;
+
+select * from employees where officeCode = 1 or officeCode = 4 and jobTitle = "Sales Rep"
+
+/* see all sales rep from office code 1 or office code 4 */
+select * from employees where (officeCode = 1 or officeCode = 4) and jobTitle = "Sales Rep"
+
+/* search by a partial strings */
+select * from employees where jobTitle like "Sales%";
+
+/* searchy by a string that ends with our critera */
+select * from employees where jobTitle like "%Sales";
+/* search by a substring */
+select * from employees where jobTitle like "%man%";
